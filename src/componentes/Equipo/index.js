d8776c7b53ebcbb4e6933634ae20dce3f00d9949
SofiDevO <angiemeow89@gmail.com>
2023-08-28 22:17:22 -0600

    Modulo 04 terminado, comenzando modulo 05

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -2,33 +2,45 @@
 
 import "./Equipo.css";
 import Colaborador from "../Colaborador";
+import hexToRgba from 'hex-to-rgba';
+
 const Equipo = (props) => {
-	const { titulo, colorPrimario, colorSecundario } = props.datos;
-	const estiloFondo = { backgroundColor: colorSecundario };
+	const { titulo, colorPrimario } = props.datos;
+	const estiloFondo = { backgroundColor: hexToRgba(colorPrimario, 0.6)};
 	const estiloTitulo = { borderColor: colorPrimario };
-	const { colaboradores, eliminarColaborador } = props;
+	const { colaboradores, eliminarColaborador, actualizarColor } = props;
 
-	console.log(colaboradores.length > 0);
+	
 
-	return (
-		<>
-			{colaboradores.length > 0 && (
-				<section className="equipo" style={estiloFondo}>
-					<h3 style={estiloTitulo}>{titulo} </h3>
-					<div className="colaboradores">
-						{colaboradores.map((colaborador, index) => (
-							<Colaborador
-								datos={colaborador}
-								key={index}
-								colorPrimario={colorPrimario}
-								eliminarColaborador={eliminarColaborador}
+	return <>
+				{
+					colaboradores.length > 0 && (
+						<section className="equipo" style={estiloFondo}>
+							<input
+								type="color"
+								className="input-color"
+								value={hexToRgba(colorPrimario, 0.6)}
+								onChange={(event)=>{ 
+									actualizarColor(event.target.value, titulo)
+								}}
+								
 							/>
-						))}
-					</div>
-				</section>
-			)}
-		</>
-	);
+							<h3 style={estiloTitulo}>{titulo} </h3>
+							<div className="colaboradores">
+								{colaboradores.map((colaborador, index) => (
+									<Colaborador
+										datos={colaborador}
+										key={index}
+										colorPrimario={colorPrimario}
+										eliminarColaborador={eliminarColaborador}
+									/>
+								))}
+							</div>
+						</section>
+					)
+				}
+			</>
+	
 };
 
 export default Equipo;
