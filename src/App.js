a3456bf63607f6517733cc00f88102d0cfb6246f
SofiDevO <angiemeow89@gmail.com>
2023-11-07 09:38:24 -0600

    :rocket: Add new cards

--- src/App.js
+++ src/App.js
@@ -10,13 +10,21 @@ import Footer from "./componentes/Footer";
 function App() {
 	const [mostrarFormulario, actualizarMostrar] = useState(false)
 	const [colaboradores, actualizarColaboradores] = useState([
-		{
+	{
 		id: uuidv4(),
-		equipo:"Front End",
+		equipo:"Programación",
 		foto:"https://github.com/SofiDevO.png",
 		nombre:"Angela Sofía Osorio",
-		puesto: "Beta One",
+		puesto: "One Helper",
 		fav:true
+	},
+	{
+		id: uuidv4(),
+		equipo:"Programación",
+		foto:"https://github.com/nerddiana.png",
+		nombre:"Diana Nerd ",
+		puesto: "FullStack Developer",
+		fav:false
 	},
 	{
 		id: uuidv4(),
@@ -28,11 +36,83 @@ function App() {
 	},
 	{
 		id: uuidv4(),
+		equipo:"Front End",
+		foto:"https://github.com/SofiDevO.png",
+		nombre:"Angela Sofía Osorio",
+		puesto: "frontend Developer",
+		fav:true
+	},
+	{
+		id: uuidv4(),
+		equipo:"Front End",
+		foto:"https://www.show.news/__export/1648679767591/sites/debate/img/2022/03/30/dwayne-1.jpg_423682103.jpg",
+		nombre:"La Roca",
+		puesto: "Be La Roca",
+		fav:true
+	},
+	{
+		id: uuidv4(),
+		equipo:"Front End",
+		foto:"https://e.radio-grpp.io/normal/2019/11/15/523252_864504.jpg",
+		nombre:"Tony Stark",
+		puesto: "Genius, Millionaire, Playboy, Philanthropist",
+		fav:true
+	},
+	{
+		id: uuidv4(),
 		equipo:"Data Science",
+		foto:"https://i0.wp.com/wipy.tv/wp-content/uploads/2020/08/profesor-hulk.jpg?fit=1000%2C600&ssl=1",
+		nombre:"Professor Hulk",
+		puesto: "Professor",
+		fav:false
+	},
+	{
+		id: uuidv4(),
+		equipo:"Data Science",
+		foto:"https://1.bp.blogspot.com/-Gn7cboCduPE/Xv-XzNUfTbI/AAAAAAAAHqY/XWSNyfd77WwUv1uTIFPhPA7rHQibDUCDwCK4BGAsYHg/s690/Screenshot_13.jpg",
+		nombre:"Eliot  Alderson",
+		puesto: "Data scientist",
+		fav:true
+	},
+	{
+		id: uuidv4(),
+		equipo:"Devops",
+		foto:"https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Doc_Brown.JPG/220px-Doc_Brown.JPG",
+		nombre:"Emmet Brown",
+		puesto: "Instructor",
+		fav:false
+	},
+	{
+		id: uuidv4(),
+		equipo:"Devops",
+		foto:"https://e.radio-grpp.io/normal/2019/11/15/523252_864504.jpg",
+		nombre:"Tony Stark",
+		puesto: "Genius, Millionaire, Playboy, Philanthropist",
+		fav:true
+	},
+	{
+		id: uuidv4(),
+		equipo:"Devops",
+		foto:"https://i0.wp.com/wipy.tv/wp-content/uploads/2020/08/profesor-hulk.jpg?fit=1000%2C600&ssl=1",
+		nombre:"Professor Hulk",
+		puesto: "Professor",
+		fav:false
+	},
+	{
+		id: uuidv4(),
+		equipo:"Ux y Diseño",
 		foto:"https://github.com/harlandlohora.png",
 		nombre:"Harland Lohora",
 		puesto: "Instructor",
-		fav:false
+		fav:true
+	},
+	{
+		id: uuidv4(),
+		equipo:"Ux y Diseño",
+		foto:"https://e.radio-grpp.io/normal/2019/11/15/523252_864504.jpg",
+		nombre:"Tony Stark",
+		puesto: "Genius, Millionaire, Playboy, Philanthropist",
+		fav:true
 	},
 	{
 		id: uuidv4(),
@@ -44,20 +124,12 @@ function App() {
 	},
 	{
 		id: uuidv4(),
-		equipo:"Devops",
-		foto:"https://github.com/harlandlohora.png",
-		nombre:"Harland Lohora",
-		puesto: "Instructor",
-		fav:false
-	},
-	{
-		id: uuidv4(),
-		equipo:"Ux y Diseño",
-		foto:"https://github.com/harlandlohora.png",
-		nombre:"Harland Lohora",
+		equipo:"Móvil",
+		foto:"https://hips.hearstapps.com/hmg-prod/images/globos-de-oro-gambito-de-dama-mejor-serie-1614275645.jpg?crop=1.00xw:0.667xh;0,0.0207xh&resize=1200:*",
+		nombre:"Beth Harmond ",
 		puesto: "Instructor",
 		fav:true
-	}
+	},
 ]) 
 
 
